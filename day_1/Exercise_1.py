0f866d0557a971e18f3f69b48dcd791b68bc7d12
Alif <abdulalif2003@gmail.com>
2025-11-19 12:35:21 +0800

    Exercise_1

---
+++ day_1/Exercise_1.py
@@ -0,0 +1,28 @@
+# Question 1
+import sys
+print(sys.version)
+
+# Question 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+# Question 3
+print('Alif')
+print('Rahim')
+print('Malaysia')
+print("I am enjoying 30 days of python")
+
+# Question 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asanabeh','Python','Finland']))
+print(type('Alif'))
+print(type('Rahim'))
+print(type('Malaysia'))
