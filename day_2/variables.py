f25087e7a3e78b63d585843cb02f1c22d2e0c110
Alif <abdulalif2003@gmail.com>
2025-11-19 16:42:23 +0800

    day 2

---
+++ day_2/variables.py
@@ -0,0 +1,49 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Abdul'
+last_name = 'Alif'
+full_name = 'Abdul Alif'
+country = 'Malaysia'
+city = 'Sibu'
+age = 22
+year = 2003
+is_married = False
+is_true = True
+is_light_on = False
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+variable_total = num_one + num_two
+variable_diff = num_one - num_two
+variable_product = num_two*num_one
+variable_division = num_one/num_two
+variable_remainder = num_two % num_one
+variable_exp = num_one**num_two
+variable_floor_division = num_one//num_two
+
+radius = float(input('Please enter radius:'))
+pi = 3.14159
+
+# Area of a circle
+area_of_circle = pi*radius**2
+
+# Circumference of a circle
+circum_of_circle = 2*pi*radius
+
+print("Area of circle:", area_of_circle)
+print("Circumference of circle:", circum_of_circle)
