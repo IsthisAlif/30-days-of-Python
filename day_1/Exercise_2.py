6726aec86402cbf22e96277aa689f239669598e6
Alif <abdulalif2003@gmail.com>
2025-11-19 13:05:23 +0800

    day1

---
+++ day_1/Exercise_2.py
@@ -0,0 +1,14 @@
+# Question 1 (Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.)
+print(type(1))
+print(type(1.5))
+print(type(1+1j))
+
+print(type('String'))
+print(type(True))
+print(type(['Alif','Python','LockIn']))
+print(type(('Alif','Python','LockIn')))
+print(type({'Alif','Python','LockIn'}))
+print(type({'Name':'Alif'}))
+
+# Question 2 (Find an Euclidian distance between (2, 3) and (10, 8))
+print(((10 - 2)**2 + (8 - 3)**2) ** 0.5)
